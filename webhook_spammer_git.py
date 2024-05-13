7f9e96d7b9f4b4e80214d658ca821ee650a1f176
ptblank <64058049+ptblnk@users.noreply.github.com>
2024-05-13 15:35:25 +0000

    Add files via upload

---
+++ webhook_spammer_git.py
@@ -0,0 +1,37 @@
+import requests
+import threading
+import time
+
+msgs = 5 
+delay = 1
+threads = 3
+webhook = ''
+
+def spam():
+    while True:
+        headers = {
+            # Already added when you pass json=
+            # 'Content-Type': 'application/json',
+        }
+        
+        json_data = {
+            'content': 'message', #edit this to change the message
+            'username': 'username', #edit this to change the username
+        }
+        
+        response = requests.post(
+            webhook,
+            headers=headers,
+            json=json_data,
+        )
+       
+        response
+        time.sleep(delay)
+        continue
+
+def main():
+    for n in range(threads+1):
+        t = threading.Thread(target=spam)
+        t.start()
+
+main()
